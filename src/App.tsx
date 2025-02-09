2efedafea31691d3fbc1101043494c66a9d4f8d8
Evgeniy <zhekafilipp@mail.ru>
2025-02-09 20:17:41 +0300

    little fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-import { useState } from "react"
 import cls from "./App.module.css"
 import { Container } from "./FlowchartContainer/ui/Container/Container"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className={cls.root}>
       <Container />
